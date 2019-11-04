72fd58fe2a99b1d73109ce28931d2dfb9b1fabb9
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-03 18:36:27 -0800

    Build for production

---
+++ docs/precache-manifest.478933f57e204b9dd8531f09073e2436.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa73efb860857941c08ebb586f4cd405",
+    "url": "/react-hooks-redux/index.html"
+  },
+  {
+    "revision": "20aeb62cab612d599c22",
+    "url": "/react-hooks-redux/static/css/main.03590305.chunk.css"
+  },
+  {
+    "revision": "956af489b05416376c00",
+    "url": "/react-hooks-redux/static/js/2.42d579ab.chunk.js"
+  },
+  {
+    "revision": "20aeb62cab612d599c22",
+    "url": "/react-hooks-redux/static/js/main.fc322d80.chunk.js"
+  },
+  {
+    "revision": "a62858e310e5ae3e0309",
+    "url": "/react-hooks-redux/static/js/runtime-main.568b593e.js"
+  }
+]);
