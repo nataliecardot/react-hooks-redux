4872800da20c3077de1813dec03841c08ac084a7
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-03 16:35:20 -0800

    Initial setup with Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+function reducer(state, action) {
+  return state;
+}
+
+const initialState = {
+  count: 0
+}
+
+// Takes a reducer function that takes care of reducing state, i.e., taking our state value as well as an action that's used to update our state, and reducing it to a single value. The first argument is the reducer, and the second argument is the initial state. If we had Redux middleware like Redux Thunk, if we were dealing with asynchronous actions, we'd pass it as an optional third argument
+const store = createStore(reducer, initialState);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Provider makes store available to all of our components within the component tree
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
+
+function Counter() {
+  return (
+    <>
+      <h2>Counter</h2>
+      <button>+</button>
+      <button>-</button>
+    </>
   );
 }
 
